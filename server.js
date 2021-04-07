64b2f7fcad54779ef6c59d9abd1615d9d0b9cf1d
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-04-08 00:18:31 +0530

    taking user I/p and marging the names between two arrays

--- server.js
+++ server.js
@@ -2,19 +2,43 @@
 require("dotenv").config()
 const request = require("request")
 const cheerio = require("cheerio")
+const readline = require('readline')
 
-// get url
-const URL = process.env.URL
+// Readline setup
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-request(URL, (err, response, html) => {
-    if(!err && response.statusCode == 200) {
-        const $ = cheerio.load(html)
-        arr = []
-        $('.title-artist em').each((i, el) => {
-            const item = $(el).text()
-            arr.push(item)  
-        })
-        // console.log(arr);
-    }
-    console.log(process.argv);
+r1.question(`What's the name of artists: `, name => {
+    // Getting the names of the Artists
+    const artists = name.split(" ")
+    r1.close()
+
+    // Get the URL and get data from site
+    // get url
+    const URL = process.env.URL
+
+    request(URL, (err, response, html) => {
+        if(!err && response.statusCode == 200) {
+            const $ = cheerio.load(html)
+            arr = []
+            $('.title-artist em').each((i, el) => {
+                const item = $(el).text()
+                arr.push(item)  
+            })
+            console.log(arr);
+            console.log(artists);
+
+            // Marge the two array and get the common items
+            const mailArtists = []
+            for(let i = 0; i < artists.length; i++) {
+                if(arr.includes(artists[i])) {
+                    // console.log(artists[i]);
+                    mailArtists.push(artists[i])
+                }
+            }
+            
+        }
+    })
 })
